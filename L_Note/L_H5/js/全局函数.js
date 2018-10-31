f1159e3d532a787cbfc6f8ded96852072f9450be
pishuiwei <823598326@qq.cpm>
2018-10-31 22:57:46 +0800

    change

---
+++ L_Note/L_H5/js/全局函数.js
@@ -0,0 +1,9 @@
++
+	eval("alert('hi')"); //把字符串解析为JS代码
+	Person.call(o, "Kristen", 25, "Nurse"); //
+	person.hasOwnProperty("name"); //判断属性是否在对象中
+	
+	
+	new worker("jsFile")
+	requestAnimationFrane()
+	cancelRequestAnimationFrame()
