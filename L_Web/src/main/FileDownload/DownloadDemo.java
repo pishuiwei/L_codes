cb77720b1bc4e67e79e3b7844437b153f605afb3
pishuiwei <823598326@qq.cpm>
2018-11-02 00:37:47 +0800

    error

---
+++ L_Web/src/main/FileDownload/DownloadDemo.java
@@ -0,0 +1,37 @@
+package main.FileDownload;
+
+import javax.servlet.*;
+import javax.servlet.annotation.*;
+import javax.servlet.http.*;
+import java.io.*;
+
+//@WebServlet("demo")
+public class DownloadDemo extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType(this.getServletContext().getMimeType("pom.xml"));
+        resp.setHeader("Content-Disposition", "attachment;filename=pom.xml");
+
+//        String fileName = req.getParameter("filename");
+        String path = this.getServletContext().getRealPath("pom.xml");
+//        String path = this.getServletContext().getMimeType(fileName);
+
+        OutputStream out = resp.getOutputStream();
+        InputStream in = new FileInputStream(path);
+        byte[] buffer = new byte[1024];
+        int len = 0;
+        while((len=in.read(buffer)) != -1) {
+            out.write(buffer, 0, len);
+        }
+
+//        out.close();
+        in.close();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+}
+
