612d130338dc58c208d255691affd215c3063f16
pishuiwei <823598326@qq.cpm>
2018-11-18 14:09:02 +0800

    基础知识

---
+++ L_Java/java/_note/A.java
@@ -0,0 +1,19 @@
+package _note;
+
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.AtomicReference;
+
+public class A {
+
+    public static void main(String[] args) {
+        AtomicLong atomicLong = new AtomicLong(10);
+
+        String s = "123456";
+        AtomicReference<String> atomicReference = new AtomicReference<>(s);
+
+        System.out.println(atomicReference.get());
+
+        System.out.println(atomicLong);
+    }
+
+}
