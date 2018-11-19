1688f4364122c2eea50bf7ef1b9eef8aa260d489
pishuiwei <823598326@qq.cpm>
2018-11-20 00:13:44 +0800

    class

--- L_Java/java/_note/A.java
+++ L_Java/java/_note/A.java
@@ -1,23 +1,9 @@
 package _note;
 
-import java.util.concurrent.atomic.AtomicLong;
-import java.util.concurrent.atomic.AtomicReference;
-
-public class A {
+public class A extends  Thread{
 
     public static void main(String[] args) {
-        AtomicLong atomicLong = new AtomicLong(10);
 
-        String s = "123456";
-        AtomicReference<String> atomicReference = new AtomicReference<>(s);
-
-        System.out.println(atomicReference.get());
-
-        System.out.println(atomicLong);
-
-
-
-        Thread thread = new Thread("");
     }
 
 }
