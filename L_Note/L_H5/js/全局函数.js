a23f90fa00e2adf18a8746b0b8c9a87ef222c34c
pishuiwei <823598326@qq.cpm>
2019-05-06 21:04:38 +0800

    js

--- L_Note/L_H5/js/全局函数.js
+++
@@ -1,9 +0,0 @@
-+
-	eval("alert('hi')"); //把字符串解析为JS代码
-	Person.call(o, "Kristen", 25, "Nurse"); //
-	person.hasOwnProperty("name"); //判断属性是否在对象中
-	
-	
-	new worker("jsFile")
-	requestAnimationFrane()
-	cancelRequestAnimationFrame()
