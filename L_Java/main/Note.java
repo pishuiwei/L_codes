d2341b0f883d81103cfb5572b4b2093ed59f984f
pishuiwei <823598326@qq.cpm>
2019-05-11 15:27:51 +0800

    API格式转化

--- L_Java/main/Note.java
+++ L_Java/main/Note.java
@@ -1,7 +1,41 @@
+import java.io.*;
+
 public class Note {
 
     public static void main(String[] args) {
-        
+        form();
+    }
+
+    public  static void form() {
+        String path = Note.class.getResource("/").getPath();
+        File file = new File(path + "s");
+        try {
+            FileReader fileReader = new FileReader(file);
+            BufferedReader br = new BufferedReader(fileReader);
+            String line;
+            int i = 1;
+            StringBuffer sb = new StringBuffer();
+            sb.append("|方法|描述|\r\n");
+            sb.append("|:----|:----|\r\n");
+            while ((line = br.readLine()) != null) {
+                if (i%2 == 1) {
+                    sb.append("|").append(line);
+                }
+                if (i%2 == 0) {
+                    sb.append("|").append(line).append("|").append("\r\n");
+                }
+                i++;
+            }
+
+            FileWriter fw = new FileWriter("C:\\Pw\\project\\L_codes\\L_Java\\main\\t");
+            fw.write(sb.toString());
+            fw.flush();
+            fw.close();
+//            System.out.println(sb);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
 }
